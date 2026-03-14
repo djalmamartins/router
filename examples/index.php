d730cf4211bc9556d76685c1236f84c8af9436c2
Djalma Martins <djalma-martins@hotmail.com>
2026-03-14 12:59:41 -0300

    Initial project structure

---
+++ examples/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Organic\Router\Router;
+
+$router = new Router();
+
+var_dump($router);
---
+++ src/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Organic\Router;
+
+class Router
+{
+    public function __construct()
+    {
+        // router core
+    }
+}
